698218076789b568d0f8662f9f29b568b45257d4
digitalneoplasm <digitalneoplasm@gmail.com>
2024-03-26 10:08:59 -0400

    3/25/24

--- src/main/java/csc241/ds/SinglyLinkedList.java
+++ src/main/java/csc241/ds/SinglyLinkedList.java
@@ -23,22 +23,40 @@ package csc241.ds;
  * - int size() - number of elements in the list.
  */
 
-public class SinglyLinkedList {
-    private Node head;
+public class SinglyLinkedList<E> {
+    private Node<E> head;
     private int nrOfElements = 0;
 
     public SinglyLinkedList() {}
 
-    public boolean add(String s) {
+    public boolean add(E s) {
         // First elt in the linked list (head == null)
         if (head == null) {
-            head = new Node(s);
+            head = new Node<>(s);
         }
         // Otherwise...
         else {
-            head = new Node(s, head);
+            head = new Node<>(s, head);
         }
         nrOfElements++;
+        return true;
+    }
+
+    public boolean add(int i, E data) {
+        if (i < 0 || i > nrOfElements) {
+            throw new IndexOutOfBoundsException();
+        }
+        // Case 1: i == 0
+        if (i == 0)
+            add(data);
+
+        // Case 2: i > 0
+        if (i > 0) {
+            // Get the node before the one at index i.
+            Node<E> before = getNodeAtIndex(i-1);
+            addAfter(before, data);
+        }
+
         return true;
     }
 
@@ -57,30 +75,30 @@ public class SinglyLinkedList {
 //            temp = temp.next; // Go to the next node.
 //        }
 
-        for (Node temp = head; temp != null; temp = temp.next){
+        for (Node<E> temp = head; temp != null; temp = temp.next){
             result.append(temp.data).append(" ");
         }
 
         return result.toString();
     }
 
-    private Node getNodeAtIndex(int i){
-        Node temp = head;
+    private Node<E> getNodeAtIndex(int i){
+        Node<E> temp = head;
         for (int j = 0; j < i; j++){
             temp = temp.next; // Move to the next node.
         }
         return temp;
     }
 
-    private void addAfter(Node previous, String data){
+    private void addAfter(Node<E> previous, E data){
 //        Node oldNext = previous.next;
 //        Node newNode = new Node(data);
 //        newNode.next = oldNext;
 //        previous.next = newNode;
         // previous -> newNode -> oldNext
-        Node newNode = new Node(data, previous.next);
+        Node<E> newNode = new Node<>(data, previous.next);
         previous.next = newNode;
-
+        nrOfElements++;
     }
 
     // 2 kinds of nested classes:
@@ -88,15 +106,15 @@ public class SinglyLinkedList {
     //                - don't rely on any instance of the outer class.
     // inner class - does rely on an instance of the outer class, can
     //               access all members.
-    private static class Node {
-        private String data;
-        private Node next;
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
 
-        public Node(String d){
+        public Node(E d){
             this.data = d;
         }
 
-        public Node(String d, Node n) {
+        public Node(E d, Node<E> n) {
             this.data = d;
             this.next = n;
         }
--- src/main/java/csc241/testers/LLTester.java
+++ src/main/java/csc241/testers/LLTester.java
@@ -1,14 +1,33 @@
 package csc241.testers;
 
 import csc241.ds.SinglyLinkedList;
+import csc241.exercises.Rectangle;
 
 public class LLTester {
     public static void main(String[] args) {
-        SinglyLinkedList ll = new SinglyLinkedList();
+        SinglyLinkedList<String> ll = new SinglyLinkedList<>();
 
         for (int i = 0; i < 10; i++){
             ll.add(""+(char)('a'+i));
             System.out.println(ll.toString());
         }
+
+        ll.add(10, "z");
+        System.out.println(ll);
+        ll.add(0, "A");
+        ll.add(5, "M");
+        System.out.println(ll);
+
+        SinglyLinkedList<Rectangle> rects = new SinglyLinkedList<>();
+        rects.add(new Rectangle(0,0,10,20));
+        rects.add(new Rectangle(5,5,15,25));
+
+        System.out.println(rects);
+
+        SinglyLinkedList<SinglyLinkedList<String>> ssl = new SinglyLinkedList<>();
+        ssl.add(ll);
+        ssl.add(ll);
+
+        System.out.println(ssl);
     }
 }
